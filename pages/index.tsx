f35d92315336ba4a01d683e4e5ef149531893afd
formex@2690 <mohammed.ibrahim3210@gmail.com>
2023-03-25 22:42:55 +0530

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '@component/styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+      </Head>
+      <main>
+        <div>
+        </div>
+
+      </main>
+    </>
+  )
+}
